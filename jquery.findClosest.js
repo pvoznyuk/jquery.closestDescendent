e6405733b2208efbf9ec663162be84e084863bed
Pavlo Vozniuk <antipsih@ukr.net>
2016-03-02 16:38:27 +0200

    Create jquery.findClosest.js

---
+++ jquery.findClosest.js
@@ -0,0 +1,15 @@
+// Like jQuery .closest() function but traversing descendants.
+
+(function($) {
+  $.fn.closest_descendent = function(filter) {
+    var $found = $(),
+        $currentSet = this; // Current place
+    while ($currentSet.length) {
+      $found = $currentSet.filter(filter);
+      if ($found.length) break;  // At least one match: break loop
+      // Get all children of the current set
+      $currentSet = $currentSet.children();
+    }
+    return $found.first(); // Return first match of the collection
+  }
+})(jQuery);
